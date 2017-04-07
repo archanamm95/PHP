9e41dd6dff2d4843532d3fa38927929b3fbef999
archanamm95 <archanamm95@gmail.com>
2017-04-07 12:41:25 +0530

    Add files via upload

---
+++ php_7.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$string1 = "Hello";
+$string2 = "baabtra";
+$variable = 'mentoring parnter';
+echo "Concatenation of two strings: ".$string1." ".$string2."<br>";
+$variable2=$string1." ".$string2." ".$variable;
+echo "Concatenation of string and variable: ".$variable2;
+
+
+//$var2 = "Name&#92;branch&#92;".$var1;
+?>  
+
+</body>
+</html>
